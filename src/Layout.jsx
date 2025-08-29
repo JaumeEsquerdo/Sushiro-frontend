89271433a6c5058804a2c9c827eb5cf9f2df002f
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-08-29 10:47:51 +0200

    comentar header

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,13 +1,13 @@
 
 import { Outlet } from 'react-router'
-import Header from '@/components/Header'
+// import Header from '@/components/Header'
 
 
 function Layout() {
 
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Outlet />
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import router from '@/lib/routes/router'
 
-import { UIProvider } from './context/UIContext'
+import { UIProvider } from './context/UIContext'/* UIProvider es de mi context global */
 
 /* css */
 import '@/css/index.css'
@@ -21,3 +21,5 @@ createRoot(document.getElementById('root')).render(
     </UIProvider>
   </StrictMode>,
 )
+
+
