4e04bd7d4651a39ae876f84557b12dfe5ae67ef5
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-11-24 13:28:28 +0100

    cambiar skeleton de galeria por un spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,15 @@
+export const Spinner = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        minHeight: "400px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="Spinner" />
+    </div>
+  );
+};
